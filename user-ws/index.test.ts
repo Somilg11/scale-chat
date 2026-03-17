e3927cd7d971886cec18e25fdf790040e736ceb6
Somil Gupta <gsomil93@gmail.com>
2026-03-17 15:15:31 +0530

    simple test passing

--- user-ws/index.test.ts
+++ user-ws/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, test} from "bun:test";
+import { describe, expect, test } from "bun:test";
 
 const BACKEND_URL = "ws://localhost:8080";
 
@@ -8,24 +8,21 @@ describe("Chat application", () => {
         const ws2 = new WebSocket(BACKEND_URL);
 
         // make sure sockets are connected
-        
         await new Promise<void>((resolve, reject) => {
-            let count = 1;
+            let count = 0;
             ws1.onopen = () => {
-                count += 1;
-                if(count == 2){
+                count = count + 1;
+                if (count == 2) {
                     resolve();
                 }
             }
             ws2.onopen = () => {
-                count += 1;
-                if(count == 2){
+                count = count + 1;
+                if (count == 2) {
                     resolve();
                 }
             }
         });
-
-        console.log('done');
 
         ws1.send(JSON.stringify({
             type: "join-room",
@@ -35,5 +32,36 @@ describe("Chat application", () => {
             type: "join-room",
             room: "Room 1"
         }));
+
+        // small delay to avoid race condition
+        await new Promise((r) => setTimeout(r, 100));
+
+        await new Promise<void>((resolve, reject) => {
+            const timeout = setTimeout(() => {
+                reject(new Error("Message not received"));
+            }, 3000);
+
+            ws2.onmessage = ({ data }) => {
+                try {
+                    const parsed = JSON.parse(data.toString());
+
+                    expect(parsed.type).toBe("chat");
+                    expect(parsed.room).toBe("Room 1");
+                    expect(parsed.message).toBe("Hello");
+
+                    clearTimeout(timeout);
+                    resolve();
+                } catch (err) {
+                    reject(err);
+                }
+            }
+            ws1.send(JSON.stringify({
+                type: "chat",
+                room: "Room 1",
+                message: "Hello"
+            }));
+        })
+        ws1.close();
+        ws2.close();
     });
 })
